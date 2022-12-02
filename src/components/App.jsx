0bbe59c46ebb2a3ef58318809cad3caa0e55c109
Imantha Thuduwage <76580224+Imantha-Thuduwage@users.noreply.github.com>
2022-12-02 11:08:01 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function CreateEntry(detail) {
+  return (
+    <Entry
+      key={detail.id}
+      emoji={detail.emoji}
+      name={detail.name}
+      meaning={detail.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(CreateEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
